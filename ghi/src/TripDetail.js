2491a7e999d339d0ec060ef758b0b6f799d073bd
Alex Reiman <reiman.alex@gmail.com>
2023-01-20 11:04:52 -0700

    TripDetail now functioning

--- ghi/src/TripDetail.js
+++ ghi/src/TripDetail.js
@@ -1,33 +1,41 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
 
-async function TripDetail() {
-
+function TripDetail() {
     let {tripId} = useParams();
+    const [trip, setTrip] = useState('');
+    const navigate = useNavigate();
 
-    const url = `${process.env.REACT_APP_TRAVEL_MAKERS}/api/trips/${tripId}`;
-    const response = await fetch(url, {
-        method: 'get',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    if (response.ok) {
-        let data=await response.json();
-        // console.log("data", data)
-        return (
+    const getData = async () => {
+        const url = `${process.env.REACT_APP_TRAVEL_MAKERS}/api/trips/${tripId}`;
+        const response = await fetch(url, {
+            method: 'get',
+            credentials: 'include',
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        });
+        const data=await response.json();
+        setTrip(data)
+    }
+
+    useEffect(() => {
+        getData();
+    }, []
+    )
+
+    return (
+        <div>
             <div>
-                <div>
-                    <h1>{data.trip_name}</h1>
-                    <h2>{data.destination}</h2>
-                    <h3>{data.start_date}-{data.end_date}</h3>
-                    <h4>{data.num_people} "people are joining you in this trip"</h4>
-                </div>
-
-                <button className='button is-primary'>Add a flight</button>
+                <h1>{trip.trip_name}</h1>
+                <h2>{trip.destination}</h2>
+                <h3>{trip.start_date}-{trip.end_date}</h3>
+                <h4>{trip.num_people} people are joining you in this trip</h4>
             </div>
-        );
-    };
+
+            <button className='button is-primary'onClick={() => {navigate("/flights")}}>Add a flight</button>
+        </div>
+    );
 };
 
 export default TripDetail;
