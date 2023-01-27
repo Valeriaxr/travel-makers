e358a9c0a65b95017b6ae5b9a6ea414d018c9326
Valeria  Ramirez <valeriaxramirez@gmail.com>
2023-01-27 13:44:21 -0800

    new logo/background

--- ghi/src/TripsForm.js
+++ ghi/src/TripsForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import BulmaInput from './BulmaInput';
 import { useAddTripMutation } from './store/tripsApi';
 import ErrorNotification from "./ErrorNotification";
+import vidd from './vids/trip.mp4';
 
 
 function TripForm() {
@@ -27,63 +28,68 @@ function TripForm() {
     }
 
     return (
-     <div className="container" style={{
-            display: "flex", 
-            alignItems: "center", 
-            justifyContent: "center",
-            position: "absolute",
-            top: "40%",
-            left: "50%",
-            transform: "translate(-50%, -40%)",
-            backgroundColor: "#d3c6a3",
-            border: "1.75px solid black",
-            width: '250px',
-            height: "365px",
-
-        }}>
-      <div className="columns is-centered">
-        <div className="column is-one-third">
-          <ErrorNotification error={error} />
-          <form onSubmit={handleSubmit}>
-          <BulmaInput
-              label="Trip name"
-              id="name"
-              placeholder="Summer Trip"
-              value={name}
-              onChange={setName} />
-            <BulmaInput
-              label="Destination"
-              id="destination"
-              placeholder="Hawaii"
-              value={destination}
-              onChange={setDestination} />
-            <BulmaInput
-              label="Start Date"
-              id="start"
-              type="date"
-              placeholder="YYYY-MM-DD"
-              value={start}
-              onChange={setStart} />
-            <BulmaInput
-              label="End Date"
-              id="end"
-              type="date"
-              placeholder="YYYY-MM-DD"
-              value={end}
-              onChange={setEnd} />
-            <BulmaInput
-              label="Number of people"
-              id="people"
-              placeholder="2"
-              value={people}
-              onChange={setPeople} />
-            <div className="field">
-              <button className="btn btn-dark">Save</button>
-            </div>
-          </form>
-        </div>
+      <><div className='back-vid' style={{ width: "30%", height: "60%" }}>
+        <video src {...vidd} autoPlay loop muted />
       </div>
-    </div>
+
+      <div className="container" style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: "40%",
+        left: "50%",
+        transform: "translate(-40%, -40%)",
+        boxShadow: '1px 2px 5px black',
+        backgroundColor: "#f2f2f2",
+        borderRadius: 4,
+        border: "1.75px solid black",
+        width: '230px',
+        height: "375px",
+      }}>
+          <div className="columns is-centered">
+            <div className="column is-one-third">
+              <ErrorNotification error={error} />
+              <form onSubmit={handleSubmit}>
+                <BulmaInput
+                  label="Trip name"
+                  id="name"
+                  placeholder="Summer Trip"
+                  value={name}
+                  onChange={setName} />
+                <BulmaInput
+                  label="Destination"
+                  id="destination"
+                  placeholder="Hawaii"
+                  value={destination}
+                  onChange={setDestination} />
+                <BulmaInput
+                  label="Start Date"
+                  id="start"
+                  type="date"
+                  placeholder="YYYY-MM-DD"
+                  value={start}
+                  onChange={setStart} />
+                <BulmaInput
+                  label="End Date"
+                  id="end"
+                  type="date"
+                  placeholder="YYYY-MM-DD"
+                  value={end}
+                  onChange={setEnd} />
+                <BulmaInput
+                  label="Number of people"
+                  id="people"
+                  placeholder="2"
+                  value={people}
+                  onChange={setPeople} />
+                <div className="field">
+                  <button className="btn btn-dark btn-sm">Save</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div></>
     )
 }
 
